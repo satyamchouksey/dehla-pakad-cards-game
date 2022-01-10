a62b6589cab804b37697446b84d35962c84c5f69
satyam <satyam.chouksey@go-mmt.com>
2022-01-10 17:53:57 +0530

    added inhand suits sepration and sorting

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../card/Card'
 import './board.css'
-import tableImage from '../../images/table.png'
 const players =[[0,-1],[1,0],[0,1],[-1,0]]
+const acePositions=[0,13,26,39];
 
 // {/* {[...Array(52).keys()].map((i)=><Card i={i}/>)} */}
 
@@ -18,13 +18,46 @@ const Board = () => {
         }
         setDeck(tempCards);
     };
+    const sortSuitCards = (suitCards) =>{
+        return suitCards.sort((a,b)=>acePositions.includes(a)?-1:acePositions.includes(b)?1:b-a)
+    }
+    const sortCards = (cards) =>{
+        let pivotHeart=0;
+        let pivotClub=0;
+        let pivotDiamond=0;
+        const sortedDeck=[];
+        cards.map(cardIndex=>{
+            if(cardIndex<13&&cardIndex>=0){
+                sortedDeck.unshift(cardIndex);
+                pivotHeart++;
+                pivotClub++;
+                pivotDiamond++;
+            }
+            else if(cardIndex<26&&cardIndex>=13){
+                sortedDeck.splice(pivotHeart,0,cardIndex);
+                pivotClub++;
+                pivotDiamond++;
+            }
+            else if(cardIndex<39&&cardIndex>=26){
+                sortedDeck.splice(pivotClub,0,cardIndex);
+                pivotDiamond++;
+            }
+            else{
+                sortedDeck.push(cardIndex);
+            }
+        })
+        return [...sortSuitCards(sortedDeck.slice(0,pivotHeart)),
+            ...sortSuitCards(sortedDeck.slice(pivotHeart, pivotClub)),
+            ...sortSuitCards(sortedDeck.slice(pivotClub, pivotDiamond)),
+            ...sortSuitCards(sortedDeck.slice(pivotDiamond))]
+    }
     useEffect(() => {
         shuffle();
     }, [])
 
     const inHandCards = (from, to, cl='') => (
             <div className={`inHandVisibleCards ${cl}`}>
-                {deck.slice(from, to).map((vCard,index)=><Card i={vCard} style={{position: 'absolute', fontSize:'20px', left: `${index*25}px`}}/>)}
+                {sortCards(deck.slice(from, to)).map((vCard,index)=><Card i={vCard} style={{position: 'absolute', fontSize:'20px', left: `${index*25}px`}}/>)}
             </div>
         )
     
@@ -34,18 +67,18 @@ const Board = () => {
     return (
         <div className="board">
             <div className="row">{/*later just keep users card open*/}
-                <div className="cardContainer0">{inHandCards(0,13, "row1")}</div>
+                {inHandCards(0,13, "row1")}
             </div>
             <div className="row">
                 <div className="col col1">
-                    {inHandCards(0,13)}
+                    {inHandCards(13,26)}
                 </div>
                 <div className="col col2">
-                    {inHandCards(0,13)}
+                    {inHandCards(26,39)}
                 </div>
             </div>
             <div className="row">
-                    {inHandCards(0,13, "row3")}
+                {inHandCards(39,52, "row3")}
             </div>
         </div>
     )
